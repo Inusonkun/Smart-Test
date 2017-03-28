009a6d91d2ff32125426daacdcd712f94e1159ee
Inusonkun <sonkun246@gmail.com>
2017-03-29 00:57:42 +0700

    commit 9

--- SmartTest/Program.cs
+++ SmartTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartTest.Students;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace SmartTest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new frmStudent());
         }
     }
 }
