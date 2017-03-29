673c0df149cea839138b65ba5f1143498528a5aa
Inusonkun <sonkun246@gmail.com>
2017-03-29 17:33:18 +0700

    commit 10

---
+++ SmartTest/SystemForm/SelectForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SmartTest.SystemForm
+{
+    partial class SelectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgrSelect = new System.Windows.Forms.DataGridView();
+            this.btSelect = new System.Windows.Forms.Button();
+            this.btClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrSelect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgrSelect
+            // 
+            this.dtgrSelect.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgrSelect.Location = new System.Drawing.Point(-2, -1);
+            this.dtgrSelect.Name = "dtgrSelect";
+            this.dtgrSelect.Size = new System.Drawing.Size(532, 167);
+            this.dtgrSelect.TabIndex = 0;
+            // 
+            // btSelect
+            // 
+            this.btSelect.Location = new System.Drawing.Point(59, 202);
+            this.btSelect.Name = "btSelect";
+            this.btSelect.Size = new System.Drawing.Size(75, 23);
+            this.btSelect.TabIndex = 1;
+            this.btSelect.Text = "Select";
+            this.btSelect.UseVisualStyleBackColor = true;
+            this.btSelect.Click += new System.EventHandler(this.btSelect_Click);
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(344, 202);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 2;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // SelectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 261);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.btSelect);
+            this.Controls.Add(this.dtgrSelect);
+            this.Name = "SelectForm";
+            this.Text = "SelectForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrSelect)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgrSelect;
+        private System.Windows.Forms.Button btSelect;
+        private System.Windows.Forms.Button btClose;
+    }
+}
---
+++ SmartTest/SystemForm/SelectForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartTest.SystemForm
+{
+    public partial class SelectForm : Form
+    {
+        DataSet ds = new DataSet();
+
+        public SelectForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btSelect_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+
+        }
+
+        public string Selected
+        {
+            get { return dtgrSelect.CurrentRow.Cells[0].Value.ToString(); }
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    e.SuppressKeyPress = true;
+                    if (btSelect.Enabled) { btSelect_Click(sender, e); }
+                    break;
+                case Keys.Escape:
+                    btClose_Click(sender, e);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
