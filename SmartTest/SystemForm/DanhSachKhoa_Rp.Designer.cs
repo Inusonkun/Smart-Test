66396aa4c1e4ac10acd6b2df73099bf5241ce77f
Inusonkun <sonkun246@gmail.com>
2017-04-02 12:12:20 +0700

    commit 12

---
+++ SmartTest/SystemForm/DanhSachKhoa_Rp.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace SmartTest.SystemForm
+{
+    partial class DanhSachKhoa_Rp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crtrpDanhSachKhoa = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // crtrpDanhSachKhoa
+            // 
+            this.crtrpDanhSachKhoa.ActiveViewIndex = -1;
+            this.crtrpDanhSachKhoa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crtrpDanhSachKhoa.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crtrpDanhSachKhoa.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crtrpDanhSachKhoa.Location = new System.Drawing.Point(0, 0);
+            this.crtrpDanhSachKhoa.Name = "crtrpDanhSachKhoa";
+            this.crtrpDanhSachKhoa.Size = new System.Drawing.Size(732, 381);
+            this.crtrpDanhSachKhoa.TabIndex = 0;
+            this.crtrpDanhSachKhoa.Load += new System.EventHandler(this.crtrpDanhSachKhoa_Load);
+            // 
+            // DanhSachKhoa_Rp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 381);
+            this.Controls.Add(this.crtrpDanhSachKhoa);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "DanhSachKhoa_Rp";
+            this.Text = "Danh s¸ch khoa";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crtrpDanhSachKhoa;
+    }
+}
---
+++ SmartTest/SystemForm/DanhSachKhoa_Rp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartTest.SystemForm
+{
+    public partial class DanhSachKhoa_Rp : Form
+    {
+        public DanhSachKhoa_Rp()
+        {
+            InitializeComponent();
+        }
+
+        private void crtrpDanhSachKhoa_Load(object sender, EventArgs e)
+        {
+            RpDanhSachKhoa rpt = new RpDanhSachKhoa();
+            crtrpDanhSachKhoa.ReportSource = rpt;
+        }
+    }
+}
