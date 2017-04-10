4a5834504c8d5c7759ddde70edc2b5269257664b
Inusonkun <sonkun246@gmail.com>
2017-04-11 01:56:27 +0700

    commit 15

---
+++ SmartTest/SystemForm/SUBJECT/frmSelect.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SmartTest.SystemForm.SUBJECT
+{
+    partial class frmSelect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgrvSelect = new System.Windows.Forms.DataGridView();
+            this.btSelect = new System.Windows.Forms.Button();
+            this.btClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrvSelect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgrvSelect
+            // 
+            this.dtgrvSelect.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgrvSelect.Location = new System.Drawing.Point(1, 0);
+            this.dtgrvSelect.Name = "dtgrvSelect";
+            this.dtgrvSelect.Size = new System.Drawing.Size(453, 153);
+            this.dtgrvSelect.TabIndex = 0;
+            this.dtgrvSelect.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btSelect
+            // 
+            this.btSelect.Location = new System.Drawing.Point(119, 173);
+            this.btSelect.Name = "btSelect";
+            this.btSelect.Size = new System.Drawing.Size(75, 23);
+            this.btSelect.TabIndex = 1;
+            this.btSelect.Text = "Chọn";
+            this.btSelect.UseVisualStyleBackColor = true;
+            this.btSelect.Click += new System.EventHandler(this.btSelect_Click);
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(268, 173);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 2;
+            this.btClose.Text = "Đóng";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // frmSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 208);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.btSelect);
+            this.Controls.Add(this.dtgrvSelect);
+            this.Name = "frmSelect";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "M«n";
+            this.Load += new System.EventHandler(this.frmSelect_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrvSelect)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgrvSelect;
+        private System.Windows.Forms.Button btSelect;
+        private System.Windows.Forms.Button btClose;
+    }
+}
---
+++ SmartTest/SystemForm/SUBJECT/frmSelect.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SmartTest.DAL;
+using System.Data.SqlClient;
+using SmartTest.SystemForm;
+
+namespace SmartTest.SystemForm.SUBJECT
+{
+    public delegate void SendData(string str);
+    public partial class frmSelect : Form
+    {
+        DataAccess da;
+        DataSet dt = new DataSet();
+        public string MaSo { set; get; }
+        public frmSelect()
+        {
+            InitializeComponent();
+            da = new DataAccess();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btSelect_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+        public string Selected
+        {
+            get { return dtgrvSelect.CurrentRow.Cells[0].Value.ToString(); }
+        }
+
+        private void dtgrvSelect_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    e.SuppressKeyPress = true;
+                    if (btSelect.Enabled)
+                    {
+                        btSelect_Click(sender, e);
+                    }
+                    break;
+                case Keys.Escape:
+                    btClose_Click(sender, e);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void LoadData(string str = "")
+        {
+            string vMaSo = MaSo;
+            string sql = "SELECT MaMon, TenMon FROM [DANHSACHMONTHI]" + str + "WHERE Khoa ='" + vMaSo + "'";
+            try
+            {
+                dt = da.ExecuteAsDataSetSql(sql);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi " + ex.Message, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            BindingSource bsource = new BindingSource();
+            bsource.DataSource = dt.Tables[0];
+            dtgrvSelect.DataSource = bsource;
+            dtgrvSelect.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+        }
+
+        private void frmSelect_Load(object sender, EventArgs e)
+        {
+            Subject sj = new Subject();
+            //SendData ld = new SendData(sj.LoadData())
+            //this.send(LoadData);
+            //LoadData();
+        }
+
+    }
+}
